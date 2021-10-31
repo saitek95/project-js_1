3e569c80e3e844f4ea7b901db415e3a7a68cdd32
Ilgiz <saitek95@mail.ru>
2021-10-31 14:05:41 +0300

    work-1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-"use sctrict";
+'use strict';
 
-let und;
-console.log(und);
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const movie = prompt('Один из последних просмотренных фильмов?', ''),
+      ocenka = prompt('На сколько оцените его?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB.movies[movie] = ocenka;
+
+console.log(personalMovieDB);
